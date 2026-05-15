f0ac89ed96ef212c04934d4e791c30804a3da36e
Jaymehta-ibm <92861197+Jaymehta-ibm@users.noreply.github.com>
2026-05-15 17:48:23 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -18,7 +18,7 @@ function divideNumbers(a, b) {
   }
 
   if (b === 0) {
-    throw new Error("Division by zero is not allowed");
+    throw new Error("Division by zero is not allowed.");
   }
 
   return a / b;
