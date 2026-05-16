f338242c6859601dc7683088a6dd19bac3f73146
Jaymehta-ibm <92861197+Jaymehta-ibm@users.noreply.github.com>
2026-05-16 23:29:25 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -17,7 +17,7 @@ function processOrders(orders) {
 }
 
 const result = divideNumbers(90, 0);
-console.log("Final Result::::::::", result);
+console.log("Final Results::::::::", result);
 
 const user = null;
 console.log(getUserName(user));
