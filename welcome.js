024bc73d06aae3e98f93e9f03b17d5e1e20da9be
Jaymehta-ibm <92861197+Jaymehta-ibm@users.noreply.github.com>
2026-05-27 20:15:35 +0530

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,1 +1,31 @@
-console.log("welcome");
+function divideNumbers(a, b) {
+  return a / b;
+}
+
+function getUserName(user) {
+  return user.name.toUpperCase();
+}
+
+function processOrders(orders) {
+  let total = 0;
+
+  for (let i = 0; i < orders.length; i++) {
+    total += orders[i].price;
+  }
+
+  return total;
+}
+
+const result = divideNumbers(90, 0);
+console.log("Final datas Resultss::::::::", result);
+
+const user = null;
+console.log(getUserName(user));
+
+const orders = [
+  { price: 100 },
+  { price: "200" }, // Mixed types
+  { }
+];
+
+console.log(processOrders(orders));
